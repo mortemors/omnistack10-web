f26ab990bd976d9de055f0aad0abc5ae503e1598
Jeferson GB <jefersongb@outlook.com.br>
2020-01-20 22:32:44 -0300

    Conexão com a API e criação de componentes para melhor legibilidade de código.

--- src/App.js
+++ src/App.js
@@ -1,160 +1,41 @@
 import React, { useState, useEffect } from "react";
+import api from "./services/api";
 import "./global.css";
 import "./App.css";
 import "./Sidebar.css";
 import "./Main.css";
 
+import DevForm from "./components/DevForm";
+import DevItem from "./components/DevItem";
+
 function App() {
-  const [latitude, setLatitude] = useState("");
-  const [longitude, setLongitude] = useState("");
-  const [github_username, setGithubUsername] = useState("");
-  const [techs, setTechs] = useState("");
+  const [devs, setDevs] = useState([]);
 
   useEffect(() => {
-    navigator.geolocation.getCurrentPosition(
-      position => {
-        const { latitude, longitude } = position.coords;
-        setLatitude(latitude);
-        setLongitude(longitude);
-      },
-      err => {
-        console.log(err);
-      }
-    );
-  }, []);
+    async function loadDevs() {
+      const response = await api.get("/devs");
+      setDevs(response.data);
+    }
+    loadDevs();
+  });
 
-  async function handleAddDev(e) {
-    e.preventDefault();
+  async function handleAddDev(data) {
+    const response = await api.post("/devs", data);
+
+    setDevs([...devs, response.data]);
   }
 
   return (
     <div id="app">
       <aside>
         <strong>Cadastrar</strong>
-        <form onSubmit={handleAddDev}>
-          <div className="input-block">
-            <label htmlFor="github_username">Usuário do Github</label>
-            <input
-              name="github_username"
-              id="github_username"
-              required
-              value={github_username}
-              onChange={e => setGithubUsername(e.target.value)}
-            ></input>
-          </div>
-
-          <div className="input-block">
-            <label htmlFor="techs">Usuário do Github</label>
-            <input
-              name="techs"
-              id="techs"
-              required
-              value={techs}
-              onChange={e => setTechs(e.target.value)}
-            ></input>
-          </div>
-
-          <div className="input-group">
-            <div className="input-block">
-              <label htmlFor="latitude">Latitude</label>
-              <input
-                type="number"
-                name="latitude"
-                id="latitude"
-                required
-                value={latitude}
-                onChange={e => setLatitude(e.target.value)}
-              ></input>
-            </div>
-            <div className="input-block">
-              <label htmlFor="longitude">Longitude</label>
-              <input
-                type="number"
-                name="longitude"
-                id="longitude"
-                required
-                value={longitude}
-                onChange={e => setLongitude(e.target.value)}
-              ></input>
-            </div>
-          </div>
-          <button type="submit">Salvar</button>
-        </form>
+        <DevForm onSubmit={handleAddDev} />
       </aside>
       <main>
         <ul>
-          <li className="dev-item">
-            <header>
-              <img
-                src="https://avatars1.githubusercontent.com/u/59920794?s=460&v=4"
-                alt="imagem do usuário"
-              />
-              <div className="user-info">
-                <strong>JefersonGB</strong>
-                <span>ReactJS, React Native, Node.js</span>
-              </div>
-            </header>
-            <p>
-              Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quidem,
-              sunt quibusdam quam at temporibus quas natus similique tempore qui
-              fuga!
-            </p>
-            <a href="https://github.com/JefersonGB" alt="Teste"></a>
-          </li>
-          <li className="dev-item">
-            <header>
-              <img
-                src="https://avatars1.githubusercontent.com/u/59920794?s=460&v=4"
-                alt="imagem do usuário"
-              />
-              <div className="user-info">
-                <strong>JefersonGB</strong>
-                <span>ReactJS, React Native, Node.js</span>
-              </div>
-            </header>
-            <p>
-              Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quidem,
-              sunt quibusdam quam at temporibus quas natus similique tempore qui
-              fuga!
-            </p>
-            <a href="https://github.com/JefersonGB"></a>
-          </li>
-          <li className="dev-item">
-            <header>
-              <img
-                src="https://avatars1.githubusercontent.com/u/59920794?s=460&v=4"
-                alt="imagem do usuário"
-              />
-              <div className="user-info">
-                <strong>JefersonGB</strong>
-                <span>ReactJS, React Native, Node.js</span>
-              </div>
-            </header>
-            <p>
-              Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quidem,
-              sunt quibusdam quam at temporibus quas natus similique tempore qui
-              fuga!
-            </p>
-            <a href="https://github.com/JefersonGB"></a>
-          </li>
-          <li className="dev-item">
-            <header>
-              <img
-                src="https://avatars1.githubusercontent.com/u/59920794?s=460&v=4"
-                alt="imagem do usuário"
-              />
-              <div className="user-info">
-                <strong>JefersonGB</strong>
-                <span>ReactJS, React Native, Node.js</span>
-              </div>
-            </header>
-            <p>
-              Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quidem,
-              sunt quibusdam quam at temporibus quas natus similique tempore qui
-              fuga!
-            </p>
-            <a href="https://github.com/JefersonGB"></a>
-          </li>
+          {devs.map(dev => (
+            <DevItem key={dev._id} dev={dev} />
+          ))}
         </ul>
       </main>
     </div>
